1adb7a4d0444d81725fa972b40d90316d75f6c4c
domzgarcia-baseup <dom@baseup.co>
2021-01-26 15:18:25 +0800

    do something wrong

--- sketch.test.js
+++ sketch.test.js
@@ -2,5 +2,5 @@ const { expect } = require('@jest/globals');
 const sum = require('./sketch');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1,2)).toBe(3);
+    expect(sum(1,2)).toBe(4);
 });
